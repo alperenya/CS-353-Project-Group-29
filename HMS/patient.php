840a62b444cefe374c212bee33c7fd91db1c3cc6
alperenya <62691122+alperenya@users.noreply.github.com>
2021-05-13 23:43:08 +0300

    db updated

--- HMS/patient.php
+++ HMS/patient.php
@@ -22,8 +22,6 @@ $dep = "";
 if (array_key_exists("deps", $_POST)){
     $dep = htmlspecialchars($_POST["deps"]);
 }
-
-echo $dep;
 //show departments
 $sqlDep = "SELECT department_name FROM department";
 $resDep = $con->query($sqlDep);
@@ -31,10 +29,6 @@ $resDep = $con->query($sqlDep);
 $sqlSch = "SELECT DISTINCT date FROM schedule WHERE date NOT IN (SELECT date from schedule WHERE schedule_id IN(SELECT schedule_id FROM schedule_of WHERE person_id IN(SELECT D.person_id FROM doctors D, department_of DE WHERE D.person_id = DE.person_id AND DE.department_name = '$dep')))";
 $resSch = $con->query($sqlSch);
 
-
-while($row1 = $resSch->fetch_assoc()):
-echo $row1['date'];
-endwhile;   
 
 
 $sqlApp = "SELECT D.title, P.first_name, P.last_name, DE.department_name, A.date FROM persons P, appointment A, doctors D, department_of DE WHERE A.exam_id IN (SELECT exam_id FROM appointment_of WHERE patient_id = '$id' AND doctor_id = P.person_id AND doctor_id = D.person_id AND doctor_id = DE.person_id)";
@@ -69,78 +63,80 @@ $sqlMakeApp = ""
         </div>
     </div>
 </nav>
-<div class="row" style="margin: 0px;">
-    <div class="col">
-        <div class="container">
-            <div class="row" style="margin-top: 10%;">
-                <div class="col">
-                    <div class="row">
-                        <div class="col-xl-5">
-                            <h2>Appointments:</h2>
+<div class="patient-clean">
+    <form method="post" id="patient-form">
+        <div class="row" style="margin: 0px;">
+            <div class="col">
+                <div class="container">
+                    <div class="row" style="margin-top: 10%;">
+                        <div class="col">
+                            <div class="row">
+                                <div class="col-xl-5">
+                                    <h2>Appointments:</h2>
+                                </div>
+                                <div class="col d-xl-flex justify-content-xl-end align-items-xl-center"><select name="deps" style="margin-right: 10px;">
+                                    <optgroup label="Departments">
+                                        <?php
+                                            foreach($resDep as $m)
+                                            {
+                                            ?>
+                                                <option value="<?php echo $m['department_name'];?>"><?php echo $m['department_name'];?></option>
+                                            <?php
+                                            }
+                                        ?>
+                                    </optgroup>
+                                    </select><input type="month" style="margin-right: 10px;"><button class="btn btn-success btn-sm" type="submit"  id="search"><span>New Appointment&nbsp;</span><i class="fa fa-search"></i></button></div>
+                            </div>
                         </div>
-                        <div class="col d-xl-flex justify-content-xl-end align-items-xl-center"><select id="deps" name="deps" style="margin-right: 10px;">
-                            <optgroup label="Departments">
-                                <?php
-                                    foreach($resDep as $m)
-                                    {
-                                    ?>
-                                        <option value="<?php echo $m['department_name'];?>"><?php echo $m['department_name'];?></option>
-                                    <?php
-                                    }
-                                ?>
-                            </optgroup>
-                            </select><input type="month" style="margin-right: 10px;"><button class="btn btn-success btn-sm" type="button"  onclick="Foo()" id="search"><span>New Appointment&nbsp;</span><i class="fa fa-search"></i></button></div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>Doctor Name:</th>
+                                <th>Department</th>
+                                <th>Date</th>
+                                <th>Details</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php while($row1 = $resultApp->fetch_assoc()) : ?>
+                                <tr>
+                                    <td><?php echo $row1["title"], " ", $row1["first_name"], " ", $row1["last_name"]; ?></td>
+                                    <td><?php echo $row1["department_name"]; ?></td>
+                                    <td><?php echo $row1["date"]; ?></td>
+                                    <td><button class="btn btn-primary btn-sm" type="button"><span>Go To Details&nbsp;</span><i class="fa fa-arrow-right"></i></button></td>
+                                </tr>
+                            <?php endwhile; ?>
+                            
+
+                            <tr id="sche">
+                                <td>DOKTOR</td>
+                                <td><?php echo $dep ?></td>
+                                <td><select>
+                                        <optgroup label="Available dates:">
+                                            <?php
+                                                foreach($resSch as $m)
+                                                {
+                                                ?>
+                                                    <option value="<?php echo $m['date'];?>"><?php echo $m['date'];?></option>
+                                                <?php
+                                                }
+                                            ?>
+                                        </optgroup>
+                                    </select></td>
+                                <td><button class="btn btn-success btn-sm" type="button"><span>Make Appointment</span><i class="fa fa-check"></i></button></td>
+                            </tr>
+                            
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead>
-                    <tr>
-                        <th>Doctor Name:</th>
-                        <th>Department</th>
-                        <th>Date</th>
-                        <th>Details</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php while($row1 = $resultApp->fetch_assoc()) : ?>
-                        <tr>
-                            <td><?php echo $row1["title"], " ", $row1["first_name"], " ", $row1["last_name"]; ?></td>
-                            <td><?php echo $row1["department_name"]; ?></td>
-                            <td><?php echo $row1["date"]; ?></td>
-                            <td><button class="btn btn-primary btn-sm" type="button"><span>Go To Details&nbsp;</span><i class="fa fa-arrow-right"></i></button></td>
-                        </tr>
-					<?php endwhile; ?>
-                    
-                    <tr style="display: none" id="sche">
-                        <td><select>
-                                <optgroup label="Available dates:">
-                                    <?php
-                                        foreach($resSch as $m)
-                                        {
-                                        ?>
-                                            <option value="<?php echo $m['date'];?>"><?php echo $m['date'];?></option>
-                                        <?php
-                                        }
-                                    ?>
-                                </optgroup>
-                            </select></td>
-                        <td><button class="btn btn-success btn-sm" type="button"><span>Make Appointment</span><i class="fa fa-check"></i></button></td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
         </div>
-    </div>
+    </form>
 </div>
 <script type="text/javascript">
-
-
-
-function Foo() {
-    document.getElementById('sche').style.display = 'block';
-}
 
 
 
