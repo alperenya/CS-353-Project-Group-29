441f6988938319c37e078e148b847c7cf73ac7a8
alperenya <62691122+alperenya@users.noreply.github.com>
2021-05-14 20:37:12 +0300

    patient date not fixed

--- HMS/patient.php
+++ HMS/patient.php
@@ -16,6 +16,44 @@ if(isset($_SESSION["newmessage"]) && $_SESSION["newmessage"] === true){
     $_SESSION["newmessage"] = false;
 }
 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    Patient($con);
+}
+
+
+
+function Patient($con)
+{
+    $sqlAppNo = "SELECT MAX(exam_id) FROM appointment_of";
+    $resAppNo = $con->query($sqlAppNo);
+    $row = $resAppNo->fetch_assoc();
+    $index = $row["MAX(exam_id)"] + 1;
+
+    $sqlDocNo ='SELECT MAX(D.person_id) FROM doctors D, department_of DE WHERE D.person_id = DE.person_id AND DE.department_name = "'.$_POST["deps"].'";';
+    $resDocNo = $con->query($sqlDocNo);
+    $row1 = $resDocNo->fetch_assoc();
+    $indexDoctor = $row1["MAX(D.person_id)"];
+
+
+    $sqlMakeApp = "INSERT INTO appointment_of VALUES('$index', ".$_SESSION['person_id'].", '$indexDoctor' );";
+    $resMakeApp = $con->query($sqlMakeApp);
+
+    $date = "";
+
+    if (array_key_exists("datepicker", $_POST)){
+    $date = htmlspecialchars($_POST["datepicker"]);
+    $sqlMakeApp1 = "INSERT INTO appointment VALUES('$index', '$date' );";
+    $resMakeApp1 = $con->query($sqlMakeApp1);
+
+    $sqlMakeApp2 = "INSERT INTO schedule VALUES('$indexDoctor', '$date', 'Appointment' );";
+    $resMakeApp2 = $con->query($sqlMakeApp2);
+    }
+
+
+}
+
+
 $id = $_SESSION['person_id'];
 $dep = "";
 
@@ -34,8 +72,7 @@ $resSch = $con->query($sqlSch);
 $sqlApp = "SELECT D.title, P.first_name, P.last_name, DE.department_name, A.date FROM persons P, appointment A, doctors D, department_of DE WHERE A.exam_id IN (SELECT exam_id FROM appointment_of WHERE patient_id = '$id' AND doctor_id = P.person_id AND doctor_id = D.person_id AND doctor_id = DE.person_id)";
 $resultApp = $con->query($sqlApp);
 
-
-$sqlMakeApp = ""
+$con->close();
 
 
 ?>
@@ -49,8 +86,14 @@ $sqlMakeApp = ""
     <title>patient</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.css">
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+
 </head>
 
 <body style="background: rgb(240,248,255);">
@@ -64,17 +107,20 @@ $sqlMakeApp = ""
     </div>
 </nav>
 <div class="patient-clean">
-    <form method="post" id="patient-form">
         <div class="row" style="margin: 0px;">
             <div class="col">
                 <div class="container">
-                    <div class="row" style="margin-top: 5%;">
+                    <div class="row" style="margin-top: 10%;">
                         <div class="col">
                             <div class="row">
                                 <div class="col-xl-5">
                                     <h2>Appointments:</h2>
                                 </div>
-                                <div class="col d-xl-flex justify-content-xl-end align-items-xl-center"><select name="deps" style="margin-right: 10px;">
+                                
+
+                                <div class="col d-xl-flex justify-content-xl-end align-items-xl-center">
+                                <form method="post" id="patient-form">
+                                <select name="deps" style="margin-right: 10px;">
                                     <optgroup label="Departments">
                                         <?php
                                             foreach($resDep as $m)
@@ -85,7 +131,10 @@ $sqlMakeApp = ""
                                             }
                                         ?>
                                     </optgroup>
-                                    </select><input type="month" style="margin-right: 10px;"><button class="btn btn-success btn-sm" type="submit"  id="search"><span>New Appointment&nbsp;</span><i class="fa fa-search"></i></button></div>
+                                    
+                                </select>
+                                <input type="date" name="datepicker" class="form-control datepicker" autocomplete="off">
+                                    <button class="btn btn-success btn-sm" type="submit"  id="search"><span>New Appointment&nbsp;</span><i class="fa fa-search"></i></button></form></div>
                             </div>
                         </div>
                     </div>
@@ -134,14 +183,23 @@ $sqlMakeApp = ""
                 </div>
             </div>
         </div>
-    </form>
 </div>
-<script type="text/javascript">
-
-
-
-
-</script>
+<!--<script type="text/javascript">
+    var disableDates = ["9-11-2019", "14-11-2019", "15-11-2019","2021-05-17"];
+      
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        beforeShowDay: function(date){
+            dmy = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+            if(disableDates.indexOf(dmy) != -1){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    });
+</script>-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
 </body>
