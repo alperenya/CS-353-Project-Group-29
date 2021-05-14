2b25121c061502cb8d4b5b1f836f598175bbf8cd
alperenya <62691122+alperenya@users.noreply.github.com>
2021-05-15 01:23:55 +0300

    patient finito

--- HMS/patient.php
+++ HMS/patient.php
@@ -17,14 +17,21 @@ if(isset($_SESSION["newmessage"]) && $_SESSION["newmessage"] === true){
 }
 
 
+$id = $_SESSION['person_id'];
+$dep = "";
+
+if (array_key_exists("deps", $_POST)){
+    $dep = htmlspecialchars($_POST["deps"]);
+}
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    Patient($con);
+    Patient($con, $id);
 }
 
 
 
-function Patient($con)
+function Patient($con, $id)
 {
+
     $sqlAppNo = "SELECT MAX(exam_id) FROM appointment_of";
     $resAppNo = $con->query($sqlAppNo);
     $row = $resAppNo->fetch_assoc();
@@ -36,30 +43,28 @@ function Patient($con)
     $indexDoctor = $row1["MAX(D.person_id)"];
 
 
-    $sqlMakeApp = "INSERT INTO appointment_of VALUES('$index', ".$_SESSION['person_id'].", '$indexDoctor' );";
-    $resMakeApp = $con->query($sqlMakeApp);
+    
 
     $date = "";
 
-    if (array_key_exists("datepicker", $_POST)){
     $date = htmlspecialchars($_POST["datepicker"]);
     $sqlMakeApp1 = "INSERT INTO appointment VALUES('$index', '$date' );";
     $resMakeApp1 = $con->query($sqlMakeApp1);
+    //echo $sqlMakeApp1;
 
     $sqlMakeApp2 = "INSERT INTO schedule VALUES('$indexDoctor', '$date', 'Appointment' );";
     $resMakeApp2 = $con->query($sqlMakeApp2);
-    }
+    //echo $sqlMakeApp2;
+    
+    $sqlMakeApp = "INSERT INTO appointment_of VALUES('$index', '$id', '$indexDoctor' );";
+    $resMakeApp = $con->query($sqlMakeApp);
+
+    
 
 
 }
 
 
-$id = $_SESSION['person_id'];
-$dep = "";
-
-if (array_key_exists("deps", $_POST)){
-    $dep = htmlspecialchars($_POST["deps"]);
-}
 //show departments
 $sqlDep = "SELECT department_name FROM department";
 $resDep = $con->query($sqlDep);
@@ -69,7 +74,7 @@ $resSch = $con->query($sqlSch);
 
 
 
-$sqlApp = "SELECT D.title, P.first_name, P.last_name, DE.department_name, A.date FROM persons P, appointment A, doctors D, department_of DE WHERE A.exam_id IN (SELECT exam_id FROM appointment_of WHERE patient_id = '$id' AND doctor_id = P.person_id AND doctor_id = D.person_id AND doctor_id = DE.person_id)";
+$sqlApp = "SELECT D.title, P.first_name, P.last_name, DE.department_name, A.date, A.exam_id FROM persons P, appointment A, doctors D, department_of DE WHERE A.exam_id IN (SELECT exam_id FROM appointment_of WHERE patient_id = '$id' AND doctor_id = P.person_id AND doctor_id = D.person_id AND doctor_id = DE.person_id) ORDER BY A.date DESC";
 $resultApp = $con->query($sqlApp);
 
 $con->close();
@@ -86,11 +91,14 @@ $con->close();
     <title>patient</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css">
+    <!--<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css">
+    
+    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>-->
+    
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.css">
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 
@@ -101,7 +109,7 @@ $con->close();
     <div class="container-fluid"><a class="navbar-brand" href="#" style="font-weight: bold;color: var(--blue);">Hospital Management System</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse d-xl-flex justify-content-xl-end" id="navcol-1">
             <ul class="navbar-nav">
-                <li class="nav-item"><button class="btn btn-primary" type="button">Log Out<i class="fa fa-sign-out" style="margin-left: 5px;"></i></button></li>
+                <li class="nav-item"><a href="logout.php" class="btn btn-primary" type="button">Log Out<i class="fa fa-sign-out" style="margin-left: 5px;"></i></a></li>
             </ul>
         </div>
     </div>
@@ -133,7 +141,7 @@ $con->close();
                                     </optgroup>
                                     
                                 </select>
-                                <input type="date" name="datepicker" class="form-control datepicker" autocomplete="off">
+                                <input type="text" name="datepicker" class="form-control datepicker" autocomplete="off">
                                     <button class="btn btn-success btn-sm" type="submit"  id="search"><span>New Appointment&nbsp;</span><i class="fa fa-search"></i></button></form></div>
                             </div>
                         </div>
@@ -154,28 +162,15 @@ $con->close();
                                     <td><?php echo $row1["title"], " ", $row1["first_name"], " ", $row1["last_name"]; ?></td>
                                     <td><?php echo $row1["department_name"]; ?></td>
                                     <td><?php echo $row1["date"]; ?></td>
-                                    <td><button class="btn btn-primary btn-sm" type="button"><span>Go To Details&nbsp;</span><i class="fa fa-arrow-right"></i></button></td>
+                                    <form action="appointment.php" method="get">
+                                    <input name="exam_id" style= "display: none;" value=<?php echo $row1["exam_id"]; ?>>
+                                    <td><button class="btn btn-primary btn-sm" type="submit" name="detail"><span>Go To Details&nbsp;</span><i class="fa fa-arrow-right"></i></button></td>
+                                    </form>
                                 </tr>
                             <?php endwhile; ?>
                             
 
-                            <tr id="sche">
-                                <td>DOKTOR</td>
-                                <td><?php echo $dep ?></td>
-                                <td><select>
-                                        <optgroup label="Available dates:">
-                                            <?php
-                                                foreach($resSch as $m)
-                                                {
-                                                ?>
-                                                    <option value="<?php echo $m['date'];?>"><?php echo $m['date'];?></option>
-                                                <?php
-                                                }
-                                            ?>
-                                        </optgroup>
-                                    </select></td>
-                                <td><button class="btn btn-success btn-sm" type="button"><span>Make Appointment</span><i class="fa fa-check"></i></button></td>
-                            </tr>
+                            
                             
                             </tbody>
                         </table>
@@ -184,13 +179,54 @@ $con->close();
             </div>
         </div>
 </div>
-<!--<script type="text/javascript">
-    var disableDates = ["9-11-2019", "14-11-2019", "15-11-2019","2021-05-17"];
+
+<?php $disableDate = array();
+    while($row = $resSch->fetch_assoc()){
+        array_push($disableDate, date("d-m-Y", strtotime($row['date'])));
+        
+    }
+
+    $finalDisableDates = "";
+
+    foreach($disableDate as &$value){
+        if(substr($value, 0, 1) == '0'){
+            $value = substr($value, 1);
+            if(substr($value, 2, 1) == '0'){
+                $first = substr($value, 0, 2);
+                $second = substr($value, 3, 6);
+                $value = $first . $second;
+            }
+        }
+        else{
+            if(substr($value, 3, 1) == '0'){
+                $first = substr($value, 0, 3);
+                $second = substr($value, 4, 6);
+                $value = $first . $second;
+            }
+        }
+        if($finalDisableDates == ""){
+            $finalDisableDates = "'$value'";
+        }
+        else{
+            $finalDisableDates .= ", '$value'";
+        }
+        
+
+    }
+        
+    echo "<script type='text/javascript'> var disableDates = [ " . $finalDisableDates . "];</script>"
+        
+?>
+
+
+<script type="text/javascript">
+
       
     $('.datepicker').datepicker({
         format: 'yyyy-mm-dd',
         beforeShowDay: function(date){
             dmy = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+            console.log(dmy);
             if(disableDates.indexOf(dmy) != -1){
                 return false;
             }
@@ -199,7 +235,7 @@ $con->close();
             }
         }
     });
-</script>-->
+</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
 </body>
